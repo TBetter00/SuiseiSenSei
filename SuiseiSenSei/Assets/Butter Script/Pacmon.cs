e981035d50e50ff6eac1d6620cc44ccebcfd8c15
PEPSEALSEA <iampep2009@gmail.com>
2024-04-23 22:56:48 +0700

    animation

--- SuiseiSenSei/Assets/Butter Script/Pacmon.cs
+++ SuiseiSenSei/Assets/Butter Script/Pacmon.cs
@@ -6,10 +6,18 @@ using UnityEngine;
 public class Pacmon : MonoBehaviour
 {
     public Movement movement {get; private set; }
+    public Animator animator;
+
+    public bool walkingUp = false;
+    public bool walkingLeft = false;
+    public bool walkingRight = false;
+    public bool walkingDown = false;
+
 
     private void Awake()
     {
         this.movement = GetComponent<Movement>();
+        animator = GetComponent<Animator>();
     }
     
     private void Update()
@@ -19,6 +27,8 @@ public class Pacmon : MonoBehaviour
         }
         else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)){
             this.movement.SetDirection(Vector2.down);
+            resetAllstage();
+            animator.SetBool("walkingDown", true);
         }
         else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)){
             this.movement.SetDirection(Vector2.left);
@@ -26,5 +36,13 @@ public class Pacmon : MonoBehaviour
         else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)){
             this.movement.SetDirection(Vector2.right);
         }
+    }
+
+    private void resetAllstage()
+    {
+        animator.SetBool("walkingDown", false);
+        animator.SetBool("walkingUp", false);
+        animator.SetBool("walkingLeft", false);
+        animator.SetBool("walkingRight", false);
     }
 }
