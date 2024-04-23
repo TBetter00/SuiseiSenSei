2628e2ca8d1271947a24d285f8c442dcfe12c5b2
BUTTER-KUN <138355962+BUTTER-KUN@users.noreply.github.com>
2024-04-23 16:28:36 +0700

    2

---
+++ SuiseiSenSei/Assets/Butter Script/GameManager.cs
@@ -0,0 +1,85 @@
+
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Enemy[] enemy;
+    public Pacmon pacmon;
+    public Transform pellets;
+
+    public int score {get; private set;}
+    public int lives {get; private set;}
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void Update()
+    {
+        if (this.lives <= 0 && Input.anyKeyDown){
+            NewGame();
+        }
+    }
+
+    private void NewGame()
+    {
+        SetScore(0);
+        SetLives(3);
+        NewRound();
+    }
+
+    private void NewRound()
+    {
+        foreach (Transform pellet in this.pellets) {
+            pellet.gameObject.SetActive(true);
+        }
+        ResetState();
+    }
+
+    private void ResetState()
+    {
+        for (int i = 0; i < this.enemy.Length; i++)
+            {
+            this.enemy[i].gameObject.SetActive(true);
+        }
+
+        this.pacmon.gameObject.SetActive(true);
+    }
+
+    private void SetScore(int score)
+    {
+        this.score = score;
+    }
+
+    private void GameOver()
+    {
+        for (int i = 0; i < this.enemy.Length; i++){
+            this.enemy[i].gameObject.SetActive(false);
+        }
+
+        this.pacmon.gameObject.SetActive(false);
+    }
+
+    private void SetLives(int lives)
+    {
+        this.lives = lives;
+    }
+
+    public void EnemyKilled(Enemy enemy)
+    {
+        SetScore(this.score + enemy.points);
+    }
+
+    public void PacmonKilled()
+    {
+        this.pacmon.gameObject.SetActive(false);
+        SetLives(this.lives - 1);
+
+        if (this.lives > 0){
+            Invoke(nameof(ResetState), 3.0f);
+        } else{
+            GameOver();
+        }
+    }
+}
--- SuiseiSenSei/Assets/Movement.cs
+++ SuiseiSenSei/Assets/Movement.cs
@@ -4,25 +4,5 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
 
-    private Rigidbody2D rb;
-
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    private void Update()
-    {
-        // Get input from arrow keys or WASD
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        // Calculate movement direction
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical).normalized;
-
-        // Move the player
-        rb.velocity = movement * moveSpeed;
-    }
 }
