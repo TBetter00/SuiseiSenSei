9b11a99b5a9c0a78e4dd2330ab04677edf73b7b3
mutelnwza <mutemallow@gmail.com>
2024-04-23 23:47:29 +0700

    animation added

--- SuiseiSenSei/Assets/Butter Script/Pacmon.cs
+++ SuiseiSenSei/Assets/Butter Script/Pacmon.cs
@@ -8,12 +8,6 @@ public class Pacmon : MonoBehaviour
     public Movement movement {get; private set; }
     public Animator animator;
 
-    public bool walkingUp = false;
-    public bool walkingLeft = false;
-    public bool walkingRight = false;
-    public bool walkingDown = false;
-
-
     private void Awake()
     {
         this.movement = GetComponent<Movement>();
@@ -24,25 +18,21 @@ public class Pacmon : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)){
             this.movement.SetDirection(Vector2.up);
+            animator.Play("up_walk");
         }
         else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)){
             this.movement.SetDirection(Vector2.down);
-            resetAllstage();
-            animator.SetBool("walkingDown", true);
+            animator.Play("down_walk");
+
         }
         else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)){
             this.movement.SetDirection(Vector2.left);
+            animator.Play("left_walk");
         }
         else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)){
             this.movement.SetDirection(Vector2.right);
+            animator.Play("right_walk");
         }
     }
 
-    private void resetAllstage()
-    {
-        animator.SetBool("walkingDown", false);
-        animator.SetBool("walkingUp", false);
-        animator.SetBool("walkingLeft", false);
-        animator.SetBool("walkingRight", false);
-    }
 }
