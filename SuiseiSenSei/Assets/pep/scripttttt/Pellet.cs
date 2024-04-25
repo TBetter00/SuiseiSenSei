16358cd69d6013fc99fcc93b2f12226ccdf5c96b
PEPSEALSEA <iampep2009@gmail.com>
2024-04-25 17:30:31 +0700

    zzz

--- SuiseiSenSei/Assets/pep/scripttttt/Pellet.cs
+++ SuiseiSenSei/Assets/pep/scripttttt/Pellet.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Pellet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int point = 10;
+
+    protected virtual void Eat()
     {
-        
+        this.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if(other.gameObject.layer == LayerMask.NameToLayer("Pacman"))
+        {
+            Eat();
+        }
     }
 }
