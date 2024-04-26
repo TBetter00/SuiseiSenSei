88ae5b2e5d99cef9a66c53c99a90ec8ac64beadd
mutelnwza <mutemallow@gmail.com>
2024-04-26 21:36:03 +0700

    anim

--- SuiseiSenSei/Assets/mutelnwza/SpreadHitbox.cs
+++ SuiseiSenSei/Assets/mutelnwza/SpreadHitbox.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 
 public class SpreadHitbox : MonoBehaviour{
     [SerializeField]private int dmg;
+    [SerializeField]private int dmgmultiplier;
+    private int finaldmg;
     private Enemy enemy;
     private bool dealingdmg;
     private BigEnemy bigEnemy;
+    private Pacmon pacmon;
 
     private void OnCollisionEnter2D (Collision2D collision)
     {
+        if (pacmon.stageMega){finaldmg=dmg*dmgmultiplier;}
+        else{finaldmg=dmg;}
+
         if(collision.gameObject.name =="Blinky"){enemy.Die();}
 
         else if(collision.gameObject.name == "Enemy_Big"){
@@ -24,7 +30,7 @@ public class SpreadHitbox : MonoBehaviour{
     private IEnumerator Attacking()
     {
         while(dealingdmg){
-            bigEnemy.TakeDamage(dmg);
+            bigEnemy.TakeDamage(finaldmg);
 
             yield return new WaitForSeconds(0.5f);
         }
