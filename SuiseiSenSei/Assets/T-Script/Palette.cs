9d97ce2fdf520be4ebed0533f1514987c838904f
TBetter00 <125780472+TBetter00@users.noreply.github.com>
2024-04-26 17:01:58 +0700

    IDK

--- SuiseiSenSei/Assets/T-Script/Palette.cs
+++ SuiseiSenSei/Assets/T-Script/Palette.cs
@@ -6,23 +6,55 @@ using UnityEngine.UI;
 public class Palette : MonoBehaviour
 {
     public GameManager GameManager;
+    public GameObject pellet;
+    public float DAYS;
+    [HideInInspector]public bool isEaten = false;
     public bool isPowerUp1;
     public GameObject PowerUp1;
+    [HideInInspector]public bool CoolDown = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("pacmon") && !isPowerUp1)
+        if (collision.gameObject.CompareTag("pacmon") && !isPowerUp1 && !isEaten)
         {
             GameManager.PelletEat(10);
-            Color objectColor = GetComponent<SpriteRenderer>().color;
-            objectColor.a = 0f;
-            GetComponent<SpriteRenderer>().color = objectColor;
+            isEaten = true;
+        }else if(collision.gameObject.CompareTag("pacmon") && isPowerUp1 && !isEaten)
+        {
+            isEaten = true;
         }
     }
     private void Update()
     {
         if(isPowerUp1 == true)
         {
-            PowerUp1.SetActive(true);
+            pellet.SetActive(false);
+            if (!isEaten)
+            {
+                PowerUp1.SetActive(true);
+                StartCoroutine(DeactivatePowerUp(DAYS));
+            }
+            else if(isEaten)
+            {
+                PowerUp1.SetActive(false);
+            }
+            
+        }else if(isPowerUp1 == false)
+        {   
+            if (!isEaten)
+            {   
+                pellet.SetActive(true);
+            }
+            if (isEaten)
+            {
+                pellet.SetActive(false);
+            }
         }
+    }
+    IEnumerator DeactivatePowerUp(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        isPowerUp1 = false;
+        Debug.Log("Deactivated");
+        PowerUp1.SetActive(false);
     }
 }
--- SuiseiSenSei/Assets/T-Script/PelletRoller.cs
+++ SuiseiSenSei/Assets/T-Script/PelletRoller.cs
@@ -5,31 +5,63 @@ using UnityEngine;
 public class PelletRoller : MonoBehaviour
 {
     public Palette[] pellet;
-    int randomIndex;
-    private void Update()
+    public int PowerUpnum;
+    public Pacmon pacmon;
+
+    private void Start()
     {
         pellet = FindObjectsOfType<Palette>();
-        if(FindAnyPowerUp() == false)
+        if (!pacmon.stageEgg)
         {
             Shuffle();
         }
     }
-    public void Shuffle()
+
+    private void Update()
     {
-        for(int i = 0; i<=pellet.Length; i++)
+        if (!FindAnyPowerUp())
         {
-            randomIndex = Random.Range(0,pellet.Length);
-            pellet[randomIndex].isPowerUp1 = true;
+            // Debug.Log("Shuffling");
+            if (!pacmon.stageEgg)
+            {
+                Shuffle();
+            }
         }
     }
+
+    public void Shuffle()
+    {
+        // Shuffle the array using Fisher-Yates shuffle algorithm
+        for (int i = pellet.Length - 1; i > 0; i--)
+        {
+            int randomIndex = Random.Range(0, i + 1);
+            Palette temp = pellet[i];
+            pellet[i] = pellet[randomIndex];
+            pellet[randomIndex] = temp;
+        }
+
+        // Assign power-ups to random pellets based on PowerUpnum
+        for (int i = 0; i < Mathf.Min(PowerUpnum, pellet.Length); i++)
+        {
+            pellet[i].isPowerUp1 = true;
+        }
+
+        // Debug.Log("Shuffling complete");
+    }
+
     public bool FindAnyPowerUp()
     {
-        for(int i = 0; i < pellet.Length; i++)
+        for (int i = 0; i < pellet.Length; i++)
         {
-            if (pellet[i].isPowerUp1)
+            if (pellet[i].isPowerUp1 && !pellet[i].isEaten)
             {
                 return true;
             }
-        }return false;
+            if (pellet[i].CoolDown)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
