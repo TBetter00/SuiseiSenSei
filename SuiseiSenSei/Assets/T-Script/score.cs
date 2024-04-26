da720f91ccb3ffa037b572e773f617e50d2941cd
TBetter00 <125780472+TBetter00@users.noreply.github.com>
2024-04-26 18:36:53 +0700

    add score system

--- SuiseiSenSei/Assets/T-Script/score.cs
+++ SuiseiSenSei/Assets/T-Script/score.cs
@@ -8,8 +8,19 @@ public class score : MonoBehaviour
 {
     public GameManager gameManager;
     public TextMeshProUGUI ScoreText;
+    public TextMeshProUGUI HSText;
     private void Update()
     {
         ScoreText.text = gameManager.score.ToString();
+        if(gameManager.score > PlayerPrefs.GetInt("HighScore", 0))
+        {
+            PlayerPrefs.SetInt("HighScore", gameManager.score);
+            HSText.text = gameManager.score.ToString();
+        }
+        else
+        {
+            HSText.text = PlayerPrefs.GetInt("HighScore" , 0).ToString();
+        }
+        
     }
 }
