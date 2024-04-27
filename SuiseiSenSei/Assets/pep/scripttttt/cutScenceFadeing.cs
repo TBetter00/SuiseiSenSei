9e6c86f5594a717eef62fa5a5794a63f88b7688d
PEPSEALSEA <iampep2009@gmail.com>
2024-04-27 11:33:21 +0700

    yoo

--- SuiseiSenSei/Assets/pep/scripttttt/cutScenceFadeing.cs
+++ SuiseiSenSei/Assets/pep/scripttttt/cutScenceFadeing.cs
@@ -10,44 +10,66 @@ public class cutScenceFadeing : MonoBehaviour
     public float fadeDuration = 1f;
     private float timer;
     private bool isFading = false;
+    private bool codeEnabled = true;
 
     private void Start()
     {
-        StartFadeTransition();
+        StartCoroutine(StartFadeTransition());
+        StartCoroutine(DisableForOneSecond());
     }
 
-    public void StartFadeTransition()
+    private IEnumerator DisableForOneSecond()
+    {
+        codeEnabled = false;
+        yield return new WaitForSeconds(1f);
+        codeEnabled = true;
+    }
+
+    IEnumerator StartFadeTransition()
     {
         if (!isFading)
         {
+            yield return new WaitForSeconds(10);
+            codeEnabled = false;
+            StartCoroutine(FadeTransition());
+        }
+    }
+
+    private void Update()
+    {
+       if (Input.anyKeyDown && codeEnabled)
+        {
+            codeEnabled = false;
             StartCoroutine(FadeTransition());
         }
     }
 
     IEnumerator FadeTransition()
     {
-        yield return new WaitForSeconds(10);
-        fadePanel.gameObject.SetActive(true);
-        isFading = true;
-        float startTime = Time.time;
-        Color startColor = fadePanel.color;
-        Color endColor = new Color(startColor.r, startColor.g, startColor.b, 1);
-
-        while (Time.time < startTime + fadeDuration)
+        if(!codeEnabled)
         {
-            float t = (Time.time - startTime) / fadeDuration;
-            fadePanel.color = Color.Lerp(startColor, endColor, t);
-            yield return null;
+            fadePanel.gameObject.SetActive(true);
+            isFading = true;
+            float startTime = Time.time;
+            Color startColor = fadePanel.color;
+            Color endColor = new Color(startColor.r, startColor.g, startColor.b, 1);
+
+            while (Time.time < startTime + fadeDuration)
+            {
+                float t = (Time.time - startTime) / fadeDuration;
+                fadePanel.color = Color.Lerp(startColor, endColor, t);
+                yield return null;
+            }
+
+            SceneManager.LoadScene(3);
+
+            while (fadePanel.color.a > 0)
+            {
+                fadePanel.color -= new Color(0, 0, 0, Time.deltaTime / fadeDuration);
+                yield return null;
+            }
+
+            isFading = false;
         }
-
-        SceneManager.LoadScene(3);
-
-        while (fadePanel.color.a > 0)
-        {
-            fadePanel.color -= new Color(0, 0, 0, Time.deltaTime / fadeDuration);
-            yield return null;
-        }
-
-        isFading = false;
     }
 }
