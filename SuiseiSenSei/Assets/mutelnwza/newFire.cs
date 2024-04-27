76aebd8d60f3cdb061600c50dafbe86d380f8bb4
TBetter00 <125780472+TBetter00@users.noreply.github.com>
2024-04-27 13:45:39 +0700

    add

---
+++ SuiseiSenSei/Assets/mutelnwza/newFire.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using System.Collections;
+
+public class NewFire : Skill{
+
+    public bool onActive;
+    [SerializeField]private float timer=1f;
+    private float force = 15f;
+    public RigidbodyConstraints2D constraints;
+    [SerializeField] private GameObject fire;
+
+    public virtual void Update()
+    {
+        if (onActive){
+            Cast();}
+    }
+
+    public override void SkillEnable()
+    {
+        base.SkillEnable();
+
+        onActive = true;
+        // Debug.Log("OnActive :" + onActive);
+        StartCoroutine(ActiveDuration());
+    }
+
+    public virtual void Cast()
+    {
+        GameObject firecast = Instantiate(fire, atk.location.position, atk.location.rotation);
+        if (atk.location == atk.Leftlocation || atk.location == atk.Rightlocation)
+        {
+            firecast.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionY;
+        }
+        if (atk.location == atk.Uplocation || atk.location == atk.Downlocation)
+        {
+            firecast.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
+        }
+        firecast.GetComponent<Rigidbody2D>().AddForce(atk.location.up*force, ForceMode2D.Impulse);
+    }
+
+    public override void SkillDisable()
+    {
+        base.SkillDisable();
+    }
+
+    protected virtual IEnumerator ActiveDuration()
+    {
+        yield return new WaitForSeconds(timer);
+        onActive = false;
+        // Debug.Log("OnActive :" + onActive);
+    }
+}
