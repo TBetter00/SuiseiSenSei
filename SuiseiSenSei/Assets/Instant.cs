b3cddfbd15f5a58239221355d53f1586974320c0
TBetter00 <125780472+TBetter00@users.noreply.github.com>
2024-05-08 21:48:41 +0700

    aaadad

--- SuiseiSenSei/Assets/Instant.cs
+++ SuiseiSenSei/Assets/Instant.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class Instant : MonoBehaviour
 {
     public GameObject testParticle;
+    public GameObject testParticle2;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
             Instantiate(testParticle, this.gameObject.transform.position, Quaternion.identity);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Instantiate(testParticle2, this.gameObject.transform.position, Quaternion.identity);
         }
     }
 }
