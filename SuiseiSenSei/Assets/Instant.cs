adcd41b9da4593ef151e6418196bc18e709800cc
TBetter00 <125780472+TBetter00@users.noreply.github.com>
2024-05-18 21:08:15 +0700

    Pinky Change

--- SuiseiSenSei/Assets/Instant.cs
+++ SuiseiSenSei/Assets/Instant.cs
@@ -6,15 +6,30 @@ public class Instant : MonoBehaviour
 {
     public GameObject testParticle;
     public GameObject testParticle2;
+    public GameObject testParticle3;
+    public GameObject testParticle4;
+    public GameObject testParticle5;
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             Instantiate(testParticle, this.gameObject.transform.position, Quaternion.identity);
         }
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             Instantiate(testParticle2, this.gameObject.transform.position, Quaternion.identity);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Instantiate(testParticle3, this.gameObject.transform.position, Quaternion.identity);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Instantiate(testParticle4, this.gameObject.transform.position, Quaternion.identity);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            Instantiate(testParticle5, this.gameObject.transform.position, Quaternion.identity);
         }
     }
 }
